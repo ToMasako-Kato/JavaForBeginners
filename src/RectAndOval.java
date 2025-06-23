4ac64bae6223caddb4bc51e58c2287e8bae4bef4
DESKTOP-1TI31AV\Sakas <sk1004yellow@gmail.com>
2025-06-23 10:40:07 +0900

    3-1 練習問題

---
+++ src/RectAndOval.java
@@ -0,0 +1,10 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		fillRect(70, 100, 40, 20);
+		fillRect(50, 120, 80, 20);
+		fillOval(60, 140, 20, 20);
+		fillOval(100, 140, 20, 20);
+	}
+
+}
--- src/Start.java
+++ src/Start.java
@@ -1,12 +1,7 @@
 
 public class Start {
 	public static void main(String[] args) {
-		int x = 2;
-		int y = 3;
-		System.out.println(x*y);
-		MyFrame f2 = new RectFrame();
-		MyFrame f3 = new RectFrame();
-		f3.setLocation(50,0);
+		MyFrame f3 = new RectAndOval();
 		
 	}
 
