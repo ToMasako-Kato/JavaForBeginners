ac3808f6bccb89a4cc2d51291518ea65912aa746
DESKTOP-1TI31AV\Sakas <sk1004yellow@gmail.com>
2025-06-23 10:19:27 +0900

    2-1、2-2 練習問題

--- src/Start.java
+++ src/Start.java
@@ -1,7 +1,13 @@
 
 public class Start {
 	public static void main(String[] args) {
-		System.out.println("坂本航大");
+		int x = 2;
+		int y = 3;
+		System.out.println(x*y);
+		MyFrame f2 = new RectFrame();
+		MyFrame f3 = new RectFrame();
+		f3.setLocation(50,0);
+		
 	}
 
 }
