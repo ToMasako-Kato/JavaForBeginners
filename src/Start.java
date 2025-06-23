e074efca7270cec9c33569721f06d3a0f5f99680
DESKTOP-1TI31AV\Sakas <sk1004yellow@gmail.com>
2025-06-23 09:40:53 +0900

    1-1 練習問題

--- src/Start.java
+++ src/Start.java
@@ -1,20 +1,7 @@
 
 public class Start {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("AAAA");
-		//MyFrame frame1 = new RectTest();
-		
-		//MyFrame frame10 = new DisplayCar();
-		//MyFrame frame11 = new DisplayFaceObject();
-		//MyFrame frame12 = new DisplayFaceVector();
-		//MyFrame frame13 = new DisplayCarVector();
-		//MyFrame frame14 = new DisplayCars();
-		//MyFrame frame15 = new DisplayCarTrain();
-		//MyFrame f = new DisplayCarsTrains();
-		//MyFrame f = new DisplayVehicleVector();
-		MyFrame f = new PlayerAnimation();
+		System.out.println("坂本航大");
 	}
 
 }
