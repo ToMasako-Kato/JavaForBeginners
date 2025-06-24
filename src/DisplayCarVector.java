ceab4f99f221ebd4b97bec610542d078e6402363
DESKTOP-1TI31AV\Sakas <sk1004yellow@gmail.com>
2025-06-24 10:15:34 +0900

    7-1、7-EX 練習問題

--- src/DisplayCarVector.java
+++ src/DisplayCarVector.java
@@ -4,7 +4,8 @@ public class DisplayCarVector extends MyFrame{
 	int n=1;
 	public void run() {
 		Vector<Car> cars=new Vector<Car>();
-		//for (n=1; n<30; n++) {
+		for (n=1; n<30; n++) 
+		{
 			cars.add(new Car(50,25,n,0));
 			
 			cars.add(new Car(50,100,n+1,0));
@@ -18,12 +19,12 @@ public class DisplayCarVector extends MyFrame{
 				clear();
 				for(int j=0; j<cars.size(); j++) {
 					cars.get(j).draw(this);
-					cars.get(j).move();
+					cars.get(j).move(this);
 				}
 				sleep(0.005);			
 			}
-			//cars.clear();
-		//}
+			cars.clear();
+		}
 		
 	}
 }
--- src/Start.java
+++ src/Start.java
@@ -5,7 +5,8 @@ public class Start {
 		//MyFrame f3 = new RectAndOval(); // 3 練習問題
 		//MyFrame f4 = new ManyFigures(); // 4 練習問題
 		//MyFrame f5 = new Animation(); // 5 練習問題
-		MyFrame f6 = new DisplayCar(); // 6 練習問題
+		//MyFrame f6 = new DisplayCar(); // 6 練習問題
+		MyFrame f7 = new DisplayCarVector(); // 7 練習問題
 	}
 
 }
